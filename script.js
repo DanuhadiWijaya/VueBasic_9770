763ec43c3203aa323909746427a966066dd349c2
DanuhadiWijaya <danuhadi.w@gmail.com>
2020-11-12 22:05:33 +0700

    Tugas Vue Basic

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ new Vue({
           npm: '180709770',
           kelas: 'PAW A',
       },
+      todos:[
+            
+      ],
       counter: 3,
       status: false,
       jawaban: false,
@@ -34,6 +37,31 @@ new Vue({
       },
       lagi(){
         location.reload();
+        },
+
+    save() {
+        let note = document.getElementById('note');
+        let title = document.getElementById('title');
+        let priority = document.getElementById('priority');
+            
+        let todo = {
+            title : title.value,
+            priority : priority.value,
+            note : note.value,
+        }
+        document.getElementById('note').value="";
+        document.getElementById('title').value="";
+        document.getElementById('priority').value="";
+        this.todos.push(todo);
+        console.log(this.todos);
     },
-  },
+    reset(){
+        document.getElementById('note').value="";
+        document.getElementById('title').value="";
+        document.getElementById('priority').value="";
+    },
+    del(index){
+        this.todos.splice(index,1);
+    },
+},
 });
